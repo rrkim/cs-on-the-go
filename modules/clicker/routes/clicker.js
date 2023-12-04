1c2b237dad02ba65dfc1ef2be3b1abbf9d31c6f2
Ryangrae Kim <dev@rrkim.com>
2023-12-04 15:57:13 +0900

    클리커 추가 및 공지사항 수정

--- modules/clicker/routes/clicker.js
+++ modules/clicker/routes/clicker.js
@@ -8,27 +8,28 @@ router.get('/', function (req, res, next) {
     ResponseUtility.renderPage(res, "modules/clicker/views/clicker");
 });
 
-
 router.get('/get-clicker-room-data', function (req, res) {
     fetch('https://clicker.gnu.ac.kr/Clicker/k/').then(response => response.text()).then((html) => {
         const $ = cheerio.load(html);
-
-        // 원하는 열의 데이터를 추출합니다.
         const data = [];
 
-        $('.clicker_libtech_table_list tr').each(function (i, row) {
-            const columns = $(row).find('td');
-            if (columns.length >= 3) {
-                data.push({
-                    roomName: $(columns[0]).text().trim(),
-                    totalCnt: $(columns[1]).text().trim(),
-                    currentRemainCnt: $(columns[2]).text().trim(),
-                    currentUsedPercent: $(columns[3]).find(".clicker_progress").text().trim().replace("%", "")
-                });
-            }
-        });
+        try {
+            $('.clicker_libtech_table_list tr').each(function (i, row) {
+                const columns = $(row).find('td');
+                if (columns.length >= 3) {
+                    data.push({
+                        roomName: $(columns[0]).text().trim(),
+                        totalCnt: $(columns[1]).text().trim(),
+                        currentRemainCnt: $(columns[2]).text().trim(),
+                        currentUsedPercent: $(columns[3]).find(".clicker_progress").text().trim().replace("%", "")
+                    });
+                }
+            });
 
-        res.json(data);
+            res.json(data);
+        } catch {
+            res.json(clickerDummyJson);
+        }
     }).catch(e => {
         res.json(clickerDummyJson);
     });
